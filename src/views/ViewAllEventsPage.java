ddd1f5254cb84178706346243cb3d51a843ae3cc
ellena <86466041+ellenaitsme@users.noreply.github.com>
2024-12-15 22:40:58 +0700

    fix table, gak sengaja ke-komen

--- src/views/ViewAllEventsPage.java
+++ src/views/ViewAllEventsPage.java
@@ -54,9 +54,7 @@ public class ViewAllEventsPage extends BorderPane implements Page {
 	
 	@Override
 	public void setLayouts() {
-//		eventIdColumn.setCellValueFactory(data -> new );
-		
-//		eventIdColumn.setCellValueFactory(new PropertyValueFactory<>("eventId"));
+		eventIdColumn.setCellValueFactory(new PropertyValueFactory<>("eventId"));
 		eventNameColumn.setCellValueFactory(new PropertyValueFactory<>("eventName"));
 		eventDateColumn.setCellValueFactory(new PropertyValueFactory<>("eventDate"));
 		eventLocationColumn.setCellValueFactory(new PropertyValueFactory<>("eventLocation"));
